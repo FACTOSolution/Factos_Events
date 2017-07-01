b32a8986eb8f8b04179bbce4d01973088b938367
ppalbuquerque <albpedro897@hotmail.com>
2017-07-01 01:40:32 -0300

    Configure proper routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :event
-  resources :academic, controller: 'events', type: 'Academic'
-  resources :cultural, controller: 'events', type: 'Cultural'
+  resources :academic, controller: 'events', type: 'Academic', only: :index
+  resources :cultural, controller: 'events', type: 'Cultural', only: :index
 end
