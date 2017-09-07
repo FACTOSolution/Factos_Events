fa6d6fcf7f2cae02c375609a8f16305894d86195
ppalbuquerque <albpedro897@hotmail.com>
2017-09-07 18:24:41 -0300

    Set default format of response to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :set_format
 
+  def set_format
+    request.format = 'json'
+  end
 end
